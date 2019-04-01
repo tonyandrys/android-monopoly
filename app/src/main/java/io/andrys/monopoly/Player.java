48bbcdae9b373dc8cf00ee46cc93dddc1bd1b496
Tony Andrys <anthony-andrys@uiowa.edu>
2019-04-01 13:00:06 -0500

    add board/tile art

---
+++ app/src/main/java/io/andrys/monopoly/Player.java
@@ -0,0 +1,17 @@
+package io.andrys.monopoly;
+
+/**
+ * null.java // Monopoly
+ * Tony Andrys (tony@andrys.io)
+ * Copyright 2019 - All rights reserved
+ */
+
+/**
+ * A Player is a participant in a game controlled by either a human or AI.
+ */
+
+public class Player {
+    String name;
+    int balance;
+
+}
